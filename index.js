0640b3de6d22f43fc161b86e3c08440d864f8a58
Edward Brosens <edward@jool.nl>
2015-02-17 11:52:41 +0100

    add dependencies

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-/**
- *
- * @type {{export: Function}}
- */
-module.exports = {
-	export: function() {
-		return "Arguments are" + process.argv.concat(',');
-	}
-};
+var through = require('through2');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
+
+// Consts
+const PLUGIN_NAME = 'gulp-nodeexport';
+
+
+module.exports.exportgit = function(options) {
+	var stream = through();
+	var msg = "Export git works";
+	console.log(msg);
+	return stream();
+}
